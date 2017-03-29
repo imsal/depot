e83bca7c1b0f40aa3892ff273546403e56dfcb13
Sal Tardibuono <s.tardibuono@live.com>
2017-03-29 13:28:58 -0700

    added line items to the cart and added a session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,23 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
+    @store_session_count = session_counter
+  end
+
+
+  private
+
+  # counts the number of times a user visits the homepage
+  def session_counter
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+
+    return session[:counter]
+
   end
 
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,6 +46,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  # test "should destroy product" do
+  #   assert_difference('Product.count', -1) do
+  #     delete product_url(@product)
+  #   end
+  #
+  #   assert_redirected_to products_url
+  # end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -53,4 +69,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+
+
 end
