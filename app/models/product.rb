87faf72b7ca57d1d27151d548ab5328edf93db5a
Sal Tardibuono <s.tardibuono@live.com>
2017-03-29 10:32:52 -0700

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,18 @@
+class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+
+  # ensures that the sellers can't set a product for $0 && ensure that price is a number
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  validates :title, uniqueness: true
+
+
+  # the allow_blank option is used to avoid from getting multiple error messages when the field is blank.
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
+
+end
